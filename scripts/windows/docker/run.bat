5b2a9bef31b7bd8d4186bc5053cf7362ef317c80
Elton Pinto <eltonp3103@gmail.com>
2025-01-09 12:45:06 -0500

    make image point to cs3210 org

--- scripts/windows/docker/run.bat
+++ scripts/windows/docker/run.bat
@@ -3,4 +3,4 @@ setlocal
 SET parent=%~dp0
 FOR %%a IN ("%parent%\..\..\..") DO SET "root=%%~fa"
 @ECHO ON
-docker run --rm -it --name="xv6" -v %root%:/xv6 -w="/xv6" jackwolfard/cs3210:latest
+docker run --rm -it --name="xv6" -v %root%:/xv6 -w="/xv6" cs3210/xv6:latest
