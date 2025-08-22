1696b93199afe114e5544e807bf6afabbb538477
byoseph3 <yosephBereketY@gmail.com>
2025-08-22 17:43:54 -0400

    Managed to get helloworld.c working.

--- user/src/lab0/helloworld.c
+++ user/src/lab0/helloworld.c
@@ -7,6 +7,17 @@ int
 main(int argc, char *argv[])
 {
   // Student code goes here
+  char buf[13]; // Just enough to read "Hello World!\n"
 
+  if (fork() == 0) {
+    close(0); // Need to utilize a fork so that we can know for a fact that fd is 0.
+              //Write and Close only take an fd (which is an integer).
+    open("tmp.txt", O_CREATE); // make is complaining that the correct flag is O_CREATE
+    write(0, "Hello World!\n", 13);
+    read(0, buf, 13);
+    write(1, buf, 13); // display 13 bytes.
+    close(0);
+  }
+  
   exit();
 }
