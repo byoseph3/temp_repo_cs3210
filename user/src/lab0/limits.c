feca8c591809c1f2dd3751b7299dcd4ef7a0a679
byoseph3 <yosephBereketY@gmail.com>
2025-08-24 02:13:03 -0400

    Finished limits.c. Still need to implement error hadning.

--- user/src/lab0/limits.c
+++ user/src/lab0/limits.c
@@ -8,6 +8,12 @@ int
 main(int argc, char *argv[])
 {
   // Student code goes here
+  int pages = 0;
 
+  while (sbrk(PGSIZE) != (void *) -1) { // had to cast this because make wsa complaining.
+    pages++;
+  }
+  
+  printf(1, "Maximum Memory Size: 0x%x\n", pages * PGSIZE);
   exit();
 }
