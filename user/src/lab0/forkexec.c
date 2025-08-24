d4945a630f04f5d62c498e1b94d97da0bdd68625
byoseph3 <yosephBereketY@gmail.com>
2025-08-24 00:55:00 -0400

    Completed forkexec.c. Need to go back and error handle correctly.

--- user/src/lab0/forkexec.c
+++ user/src/lab0/forkexec.c
@@ -7,5 +7,13 @@ main(int argc, char *argv[])
 {
   // Student code goes here
 
+  if (fork() == 0) {
+    // for (int i = 0; i < argc; i++) {
+    //   printf(1, "%s\t", argv[i]);
+    // }
+    exec("/echo", argv); // shortcut?
+    exit(); // Good practice according to TAs.
+  }
+  wait(); // Prevent any zombies
   exit();
 }
